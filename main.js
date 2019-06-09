f9968a5486e24b4624dbee2edd0bcceef4cd2276
Erin <erinedawson@gmail.com>
2019-06-08 18:45:03 -0600

    write inner loop, with explanatory comments

--- main.js
+++ main.js
@@ -7,5 +7,13 @@ let tinasFollowers = ['Jimmy Jr.', 'Gene', 'Louise'];
 let mutualFollowers = [];
 
 for(let bob = 0; bob < bobsFollowers.length; bob++){ //this is the outer loop
+  /*this is the inner loop.  every time the outer loop runs,
+  the inner loop goes through an entire rotation.  meaning, every time the bobsFollowers loop runs, tina's loop runs and checks
+  if they have any mutual friends.  if they do, that name gets pushed to the mutualFollowers array */
+  for(let tina = 0; tina < tinasFollowers.length; tina++){
+    if(bobsFollowers[b] === tinasFollowers[t]){
+      mutualFollowers.push(tinaFollowers[t]);
+    }
+  }
 
 };
