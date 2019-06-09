1942ac164432f6817f31d7ff9a70bd30cd9566e8
Erin <erinedawson@gmail.com>
2019-06-08 18:38:56 -0600

    create three arrays

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 /*practice with nested loops.
 remember, you can always refactor your code later
 to use more succint iterator variables*/
+
+let bobsFollowers = ['Linda', 'Teddy', 'Gene', 'Louise'];
+let tinasFollowers = ['Jimmy Jr.', 'Gene', 'Louise'];
+let mutualFollowers = [];
