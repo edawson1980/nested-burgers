cbdb70ef1bb215af6d3d8fa8fe5cd8e8152f044d
Erin <erinedawson@gmail.com>
2019-06-08 18:37:10 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the exercise you should refer back to when you get confused about nested loops.
---
+++ main.js
@@ -0,0 +1,3 @@
+/*practice with nested loops.
+remember, you can always refactor your code later
+to use more succint iterator variables*/
