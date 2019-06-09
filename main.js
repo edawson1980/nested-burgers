05813f6f5579311c1c94c82cb66f434c9307696e
Erin <erinedawson@gmail.com>
2019-06-08 18:41:18 -0600

    edit comment on line 9

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ let bobsFollowers = ['Linda', 'Teddy', 'Gene', 'Louise'];
 let tinasFollowers = ['Jimmy Jr.', 'Gene', 'Louise'];
 let mutualFollowers = [];
 
-for(let bob = 0; bob < bobsFollowers.length; bob++){ //this is the outer array
+for(let bob = 0; bob < bobsFollowers.length; bob++){ //this is the outer loop
 
 };
