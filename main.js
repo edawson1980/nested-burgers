711b79107d0e09409a59c46fc3fdb9a41e53a5b5
Erin <erinedawson@gmail.com>
2019-06-08 18:40:43 -0600

    write outer loop

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ to use more succint iterator variables*/
 let bobsFollowers = ['Linda', 'Teddy', 'Gene', 'Louise'];
 let tinasFollowers = ['Jimmy Jr.', 'Gene', 'Louise'];
 let mutualFollowers = [];
+
+for(let bob = 0; bob < bobsFollowers.length; bob++){ //this is the outer array
+
+};
